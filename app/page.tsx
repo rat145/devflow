e54d610c5b5756973c0f582f79ee3bf0a202f471
rat145 <ratannn21@gmail.com>
2024-11-16 20:47:46 +0530

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 const Home = () => {
   return (
-    <h1 className="h1-bold background-light850_dark100">
-      Welcome to the world of Next.js
-    </h1>
+    <>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold font-inter">Welcome to the world of Next.js</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the world of Next.js
+      </h1>
+    </>
   );
 };
 
